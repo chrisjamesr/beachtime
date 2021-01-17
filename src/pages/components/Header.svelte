bcb960e87fc3bfca0da64f916d4a124c73c7bcbe
Chris Rudolph <chrisjames.r@gmail.com>
2021-01-17 14:25:53 -0500

    add individual page components and layout components

---
+++ src/pages/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+<header>
+  <div>Home</div>
+  <div>About</div>
+  <div>Contact</div>
+  <div>Gallery</div>
+</header>
+<style>
+
+  header {
+    width: 90%;
+    display: flex;
+    flex-direction: row;
+  }
+  header div {
+    margin: 1px solid black
+    width:25%
+  }
+
+</style>
