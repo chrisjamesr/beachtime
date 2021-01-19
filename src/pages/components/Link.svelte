cf488243514e6bb692f0ebf94304e2c64cf9f8a4
Chris Rudolph <chrisjames.r@gmail.com>
2021-01-18 22:08:11 -0500

    add selectors for link attributes. center links in div

--- src/pages/components/Link.svelte
+++ src/pages/components/Link.svelte
@@ -2,17 +2,44 @@
   export let link;
 </script>
 
-<div class="header_link">
+<div class="header-link">
+
   <a href="/{link.name}">
     {link.name}
   </a>
+
 </div>
 
 <style>
-  a {
-    text-transform: capitalize;
-  }
+
   div {
-    align: center;
+    /* border: 1px solid black; */
+    width: 25%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
   }
+
+  a {
+    text-align: center;
+    margin: auto;
+    text-transform: capitalize;
+    color: black;
+    font-size: 2rem;
+  }
+
+  a:hover {
+    color: black;
+    text-decoration: none;
+  }
+
+  a:active {
+    color: black;
+    text-decoration: none;
+  }
+
+  a:visited {
+    color: black;
+  }  
+
 </style>
