fb13ca08be6bf653cf01ac848b2cfab7d60284a7
Chris Rudolph <chrisjames.r@gmail.com>
2021-01-18 22:06:26 -0500

    add height attribute:

--- src/pages/components/Header.svelte
+++ src/pages/components/Header.svelte
@@ -1,27 +1,27 @@
 <script>
+
   import Link from './Link.svelte';
-   import links from '../links.js';
-  // const links = ['about', "home"];
+  import links from '../links.js';
 
 </script>
 
 <header>
+
   {#each links as link}
     <Link link={link} />
   {/each}
+
 </header>
 
 <style>
 
   header {
     width: 90%;
+    height: 20vh;
     display: flex;
     flex-direction: row;
-    justify-content: space-around;
-  }
-  header div {
-    margin: 1px solid black;
-    width:25%;
+    justify-content: space-around;  
+    /* margin: 2% 0 */
   }
 
 </style>
