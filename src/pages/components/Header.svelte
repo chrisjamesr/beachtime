1b9a2711c7dcd4dd5a3330e5e9641e8b1981a4e9
Chris Rudolph <chrisjames.r@gmail.com>
2021-01-18 21:07:12 -0500

    iterate through links list for header link components

--- src/pages/components/Header.svelte
+++ src/pages/components/Header.svelte
@@ -1,23 +1,27 @@
 <script>
+  import Link from './Link.svelte';
+   import links from '../links.js';
+  // const links = ['about', "home"];
 
 </script>
 
 <header>
-  <div>Home</div>
-  <div>About</div>
-  <div>Contact</div>
-  <div>Gallery</div>
+  {#each links as link}
+    <Link link={link} />
+  {/each}
 </header>
+
 <style>
 
   header {
     width: 90%;
     display: flex;
     flex-direction: row;
+    justify-content: space-around;
   }
   header div {
-    margin: 1px solid black
-    width:25%
+    margin: 1px solid black;
+    width:25%;
   }
 
 </style>
