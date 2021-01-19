c3b9af3c6f7d1ffce6d35921d9663e532a6c51d5
Chris Rudolph <chrisjames.r@gmail.com>
2021-01-18 21:12:19 -0500

    Create link component

---
+++ src/pages/components/Link.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let link;
+</script>
+
+<div class="header_link">
+  <a href="/{link.name}">
+    {link.name}
+  </a>
+</div>
+
+<style>
+  a {
+    text-transform: capitalize;
+  }
+  div {
+    align: center;
+  }
+</style>
