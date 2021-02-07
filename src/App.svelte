54690aa42f35e11ab760c7c0b988f0b979d6be0d
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-06 23:41:54 -0500

    remove unnecessary component imports for temp landing page

--- src/App.svelte
+++ src/App.svelte
@@ -1,92 +1,35 @@
 <script>
-  import {Router, Route, Link} from 'svelte-routing';
-  import Header from './pages/components/Header.svelte';
-  import Footer from './pages/components/Footer.svelte';
-
-  import linklist from './pages/linkList.js';
   
-  export let url = "";
-
-  let currentComponent;
+  import Footer from './pages/components/Footer.svelte';
+  import Temp from './Temp.svelte';
    
 </script>
 
 
 <main>
-
-  <Router url="{url}">  
-
-    <section class="container">
-
-      <Header />
-
-      {#each linklist as route}
-        
-        <Route path={route.path}> <svelte:component this={route.component}/> </Route>
-
-      {/each}
-
-    </section>
-
+    <Temp/>
     <Footer/>
-
-  </Router> 
-
 </main>
 
-<style>
+<style>     
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	
+    height: 96%; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+	}	
 
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
-
-	.container {
-    margin: 0 auto;
-    min-height: 100vh;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    text-align: center;
-    width: 100%;
-    flex-direction: column;
-    max-width: 95vw;  
-  }
-
-section .title {
-  font-family: /* 'Caveat', cursive, */
-    'Dancing Script', cursive,
-    /* 'Source Sans Pro', */
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-    display: block;
-    font-weight: 700;
-    font-size: 6rem;
-    color: #35495e;
-    letter-spacing: 1px;
-    width: 80vw;  
-  }
-.coming-soon {
-  /* font-family: 'Dancing Script', cursive; */
-  font-family: 'Caveat', cursive;
-  font-weight: 400;
-  font-size: 3rem;
-  color: #35495e;
-}
 
 @media screen and (max-width: 500px) {
   .title {
