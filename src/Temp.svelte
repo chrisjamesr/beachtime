4ad844ab5cbe8e1b863b82429f71bce0a896214f
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-06 23:41:04 -0500

    remove unnecessary container divs. Add styling for text

--- src/Temp.svelte
+++ src/Temp.svelte
@@ -1,35 +1,39 @@
 <script>
   import Dots from './Dots.svelte'
-  const src = 'https://beachtimetideclocks.s3.us-east-2.amazonaws.com/resources/image0.jpeg'
 </script>
-
-<main>
-	<section class="container">
-		<h1 class="title">
-			<span>beachtime</span>
-			<Dots />
-		</h1>        
-		<br>
-		<div class="coming-soon">
-			<img {src} alt="sunset beach">
-		</div>
-	</section>
-</main>
+  
+<h1 class="title">
+  <span>beachtime</span>
+  <Dots />
+</h1>        
 
 <style>
-	main {
+
+	section {
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
+		width: 100%;
 		margin: 0 auto;
+    height: 98%;
 	}
 
-	h1 {
-		color: #ff3e00;
-		/* text-transform: uppercase; */
-		font-size: 4em;
-		font-weight: 100;
-	}
+  span {
+    font-family: 'Petit Formal Script', cursive;
+    font-weight: 700;
+    font-size: 6rem;
+    color: #35495e;
+    letter-spacing: 1px;
+    width: 80vw;  
+    text-align: center;
+  }
+
+  /* section .title {
+    font-weight: 700;
+    font-size: 6rem;
+    color: #35495e;
+    letter-spacing: 1px;
+    width: 80vw;  
+  } */
 
 	@media (min-width: 640px) {
 		main {
@@ -37,7 +41,7 @@
 		}
 	}
 
-	.container {
+	/* .container {
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
@@ -46,61 +50,12 @@
   text-align: center;
   width: 100%;
   flex-direction: column;
-  max-width: 95vw;
-  
-}
-
-.title {
-  font-family: /* 'Caveat', cursive, */
-    'Dancing Script', cursive,
-    /* 'Source Sans Pro', */
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 700;
-  font-size: 6rem;
-  color: #35495e;
-  letter-spacing: 1px;
-  width: 80vw;
-  
-}
-.coming-soon {
-  /* font-family: 'Dancing Script', cursive; */
-  font-family: 'Caveat', cursive;
-  font-weight: 400;
-  font-size: 3rem;
-  color: #35495e;
-}
-
-.coming-soon div {
-  font-size: 1rem;
-}
-
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  max-width: 95vw;  
+} */
 
 @media screen and (max-width: 500px) {
   .title {
     font-size: 5rem;  
-  }
-
-  .title h1 {
-    margin: 2.5%;
   }
 
 }
