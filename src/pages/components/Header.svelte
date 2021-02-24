a96407e3b9e6e1acecc69fa03bf74bd66345f45c
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-24 10:45:12 -0500

    modify selected class, & nav div sizing

--- src/pages/components/Header.svelte
+++ src/pages/components/Header.svelte
@@ -2,6 +2,7 @@
   import { Link } from 'svelte-routing';
   import linklist from '../linklist.js';  
   let current='';  
+  
 </script>
 
 <header>
@@ -24,22 +25,22 @@
 <style>
   header {
     width: 100%;
-    height: 20vh;    
+    height: 10vh;    
     vertical-align: center;
   }
 
   nav {
     width: 100%;
     height: 100%;
-    margin: auto;
+    /* margin: auto; */
     display: flex;
     flex-direction: row;
     justify-content: space-around;  
   }
 
   div {
-    text-align:center;
-    vertical-align: center;
+    /* text-align:center;
+    vertical-align: center; */
     width: 25%;
     display: flex;
     justify-content: center;
@@ -49,6 +50,9 @@
     font-family: 'Petit Formal Script', cursive;
     text-align: center; 
     margin: auto;
+    /* height: 100%; */
+    width: 100%;
+    /* padding: 10%; */
     text-transform: capitalize;
     text-decoration: none;
     /* color:rgb(240,88,25); */
@@ -56,31 +60,33 @@
     font-size: 2rem;
   }
 
-  :global(a):hover {
+  :global(a):hover, :active, :visited {
     color: black;
     text-decoration: none;
   }
 
-  :global(a):active {
+  /* :global(a):active {
     color: black;
-    text-decoration: none;
-  }
-
-  :global(a):visited {
-    color: black;
-    text-decoration: none;
-  }  
-  .selected > :global(a){
-    font-weight: 700;
-    font-size: 2.1rem;
-    color:rgb(240,88,25);
-    text-decoration: none;
-  }
-
-  /* .selected > :global(a){
-    font-weight: 700;
-    color:rgb(240,88,25);
     text-decoration: none;
   } */
+
+  /* :global(a):visited {
+    color: black;
+    text-decoration: none;
+  }   */
+
+  .selected {
+    font-weight: 700;
+    /* font-size: 2.25rem; */
+    /* color:rgb(240,88,25); */
+    color: black;
+    background: rgb(36,21,0);
+    background: radial-gradient(
+      circle, 
+      rgba(36,21,0,0.9332107843137255) 0%, 
+      /* rgba(201,123,13,1) 0%,  */
+      rgba(255,155,70,1) 0%, 
+      rgba(172,157,171,0) 40%);
+  }
 
 </style>
