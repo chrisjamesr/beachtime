4efec517e5a26842ba0a90a94f035e3ace153d60
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-24 21:56:30 -0500

    add middleware for body parser

--- server.js
+++ server.js
@@ -3,17 +3,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 // const page = require("./dist/bundle.js");
-
+app.use(express.json());
 app.use(cors());
 
 
 
-app.get('/test', (req,res)=> {
-  res.send({message: 'ok'});
-  debugger;
-})
+app.post('/test', (req,res)=> {
+  
+  if (req.body) {
+    res.redirect('/');
+  }
+   
+
+});
 
 app.use(express.static('public'))
 app.get('*', (req,res) => {
