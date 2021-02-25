a46637ad58dcfa1bf6136d8cef9ed9e930963fb5
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-24 20:58:48 -0500

    add relocate static folder server

--- server.js
+++ server.js
@@ -1,14 +1,46 @@
-const { createServer } = require("http");
-const app = require("./dist/App.js");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+const path = require('path');
 
-createServer((req, res) => {
-  const { html } = app.render({ url: req.url });
+// const page = require("./dist/bundle.js");
 
-  res.write(`
-    <!DOCTYPE html>
-    <div id="app">${html}</div>
-    <script src="/dist/bundle.js"></script>
-  `);
+app.use(cors());
 
-  res.end();
-}).listen(3000);
+
+
+app.get('/test', (req,res)=> {
+  res.send({message: 'ok'});
+  debugger;
+})
+
+app.use(express.static('public'))
+app.get('*', (req,res) => {
+  console.log(`request: ${req.url}`);
+  debugger;
+  // res.sendFile(path.resolve(__dirname, 'public', index.html));
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
+
+
+
+
+
+
+// const { createServer } = require("http");
+
+// createServer((req, res) => {
+//   const { html } = page.render({ url: req.url });
+
+//   res.write(`
+//     <!DOCTYPE html>
+//     <div id="app">${html}</div>
+//     <script src="/dist/bundle.js"></script>
+//   `);
+
+//   res.end();
+// }).listen(3000);
