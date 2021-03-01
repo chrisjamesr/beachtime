b3d5980a9198d10419eb47cd139e7eed0c04c151
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-28 22:22:46 -0500

    add new background opacity

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -2,18 +2,25 @@
 
 </script>
 
-  <header>
-    <h1>About</h1>
-  </header>
-<article class="content">
-  We are a father daughter duo based in Rockaway Beach, New York. <br>We're creating beautiful tide clocks to keep track of the local changing tides.
-  <br>
-  A perfect addition to your home by the sea.
-</article>
+<section>
+  <h1>About</h1>
 
+  <article class="content">
+    We are a father daughter duo based in Rockaway Beach, New York. <br>We're creating beautiful tide clocks to keep track of the local changing tides.
+    <br>
+    A perfect addition to your home by the sea.
+  </article>
+</section>
 <style>
   .content {
     line-height: 3rem;
     /* text-align: left; */
+    
+  }
+
+  section {
+    width: 100%;
+    height: 54.5%;
+    background: rgba(255, 255, 255, 0.6);
   }
 </style>
