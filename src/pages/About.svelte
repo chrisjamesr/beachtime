9eb45373bdf5370deb5b878d4db27fad54ddff27
Chris Rudolph <chrisjames.r@gmail.com>
2021-03-01 13:13:47 -0500

    add preliminary styling to patch design

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -2,25 +2,90 @@
 
 </script>
 
-<section>
-  <h1>About</h1>
+<article>
 
-  <article class="content">
-    We are a father daughter duo based in Rockaway Beach, New York. <br>We're creating beautiful tide clocks to keep track of the local changing tides.
+  <section class="content">
+    <h1 class="title">
+      Always Know The Tide
+    </h1>
+
+    <section id="about">
+      We are a father daughter duo creating beautiful tide clocks to keep track of the local changing tides.
     <br>
-    A perfect addition to your home by the sea.
-  </article>
-</section>
+      --A perfect addition to your home by the sea.
+    </section>
+    <a href="instagram.com" class="button">
+      Shop Our Instagram
+    </a>
+
+  </section>
+  <!-- <figure> -->
+    <img src="clock1.jpg" alt="Image of a tide clock">
+    <!-- <figcaption>One of our beautiful tide clocks</figcaption> -->
+  <!-- </figure> -->
+
+</article>
+
 <style>
-  .content {
-    line-height: 3rem;
-    /* text-align: left; */
-    
+   article {
+    width: 100%;
+    height: 57%;
+    background: rgba(255, 255, 255, 0.6);
+    padding: 2.5% 1.5%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
   }
 
-  section {
-    width: 100%;
-    height: 54.5%;
-    background: rgba(255, 255, 255, 0.6);
+  
+  .content {
+    height: 80%;
+    line-height: 3rem;
+    width: 42%;
+    padding: 2% 0 3% 0;
+    text-align: left;    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
   }
+  
+  img {
+    height: 95%;
+  }
+
+  a {
+    background-color: #013E48;    
+    width: 35%;
+    padding: 0 10%;
+    height: 20%;
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 15.75px;
+    line-height: 19px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: #FFFFFF;
+  }
+
+  .title  {
+    font-family: Yellowtail;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 48.75px;
+    line-height: 66px;
+    color: #013E48;
+    margin: 0;
+  }
+  #about {
+    font-family: Montserrat;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 18.75px;
+    line-height: 150%;
+    /* or 28px */
+    color: #000000;
+  }
+
 </style>
