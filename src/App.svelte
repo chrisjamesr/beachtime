984d8ca412c9180c1dd2081240713b7e8aaa7265
Chris Rudolph <chrisjames.r@gmail.com>
2021-02-28 22:21:59 -0500

    delete unused styles

--- src/App.svelte
+++ src/App.svelte
@@ -37,58 +37,25 @@
 <style>
 	main {
 		text-align: center;
-		/* padding: 1em; */
-		max-width: 240px;
-		/* margin: 0 auto; */
     height: 98%;
 	}
 
 	
 
 	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+		
 	}
 
 	.container {
-    margin: 0 auto;
-    /* min-height: 100vh; */
+    /* margin: 0 auto;     */
     height: 98%;
     display: flex;
     justify-content: flex-start;
     align-items: center;
-    text-align: center;
-    width: 100%;
+    /* text-align: center; */
     flex-direction: column;
-    max-width: 95vw;  
+    /* max-width: 95vw;   */
   }
-
-section .title {
-  font-family: /* 'Caveat', cursive, */
-    'Dancing Script', cursive,
-    /* 'Source Sans Pro', */
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-    display: block;
-    font-weight: 700;
-    font-size: 6rem;
-    color: #35495e;
-    letter-spacing: 1px;
-    width: 80vw;  
-  }
-.coming-soon {
-  /* font-family: 'Dancing Script', cursive; */
-  font-family: 'Caveat', cursive;
-  font-weight: 400;
-  font-size: 3rem;
-  color: #35495e;
-}
 
 @media screen and (max-width: 500px) {
   .title {
