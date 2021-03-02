d74d5c39fd83f6e9a3db4ed91264860235172c04
Chris Rudolph <chrisjames.r@gmail.com>
2021-03-02 10:04:47 -0500

    mdoify element padding and sizing to ensure component does not encroach on header

--- src/pages/About.svelte
+++ src/pages/About.svelte
@@ -2,87 +2,67 @@
 
 </script>
 
-<article>
+<article class="content-container">
 
   <section class="content">
-    <h1 class="title">
+
+    <div class="heading">
       Always Know The Tide
-    </h1>
+    </div>
 
     <section id="about">
       We are a father daughter duo creating beautiful tide clocks to keep track of the local changing tides.
     <br>
       --A perfect addition to your home by the sea.
     </section>
-    <a href="instagram.com" class="button">
+    <a href="https://www.instagram.com/beachtime_tideclocks/" class="button"
+      rel="noreferrer noopener" target="_blank" >
       Shop Our Instagram
     </a>
 
   </section>
-  <!-- <figure> -->
+
+  <figure>
     <img src="clock1.jpg" alt="Image of a tide clock">
     <!-- <figcaption>One of our beautiful tide clocks</figcaption> -->
-  <!-- </figure> -->
-
+  </figure> 
+  
 </article>
 
 <style>
-   article {
-    width: 100%;
-    height: 57%;
-    background: rgba(255, 255, 255, 0.6);
-    padding: 2.5% 1.5%;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-  }
-
-  
+   
   .content {
-    height: 80%;
+    height: 90%;
     line-height: 3rem;
     width: 42%;
-    padding: 2% 0 3% 0;
+    /* padding: 2% 0 3% 0; */
     text-align: left;    
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-around;
   }
+  /* .wrapper {
+    align-self: center;
+  } */
+  
   
   img {
-    height: 95%;
+    max-width: 90%;
+    margin:auto;
+    /* width: 80%; */
   }
 
-  a {
-    background-color: #013E48;    
-    width: 35%;
-    padding: 0 10%;
-    height: 20%;
-    font-family: Montserrat;
-    font-style: normal;
-    font-weight: 900;
-    font-size: 15.75px;
-    line-height: 19px;
-    display: flex;
-    align-items: center;
-    text-align: center;
-    color: #FFFFFF;
-  }
-
-  .title  {
-    font-family: Yellowtail;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 48.75px;
-    line-height: 66px;
-    color: #013E48;
+  figure {
+    /* align-items: center;  */
+    height: 100%;
     margin: 0;
   }
+
   #about {
     font-family: Montserrat;
     font-style: normal;
     font-weight: normal;
-    font-size: 18.75px;
+    font-size: 1.171875rem;
     line-height: 150%;
     /* or 28px */
     color: #000000;
